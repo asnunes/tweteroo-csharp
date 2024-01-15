8bc5eee0712754a62944b9e94aa70ff39f6ef3bc
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2024-01-15 16:52:17 -0300

    fix: do not add ssh key to agent

--- .deploy/push.sh
+++ .deploy/push.sh
@@ -5,9 +5,7 @@ pk_filepath=/tmp/pk
 # Add private key to ssh-agent
 touch $pk_filepath
 echo $PK >> $pk_filepath
-eval "$(ssh-agent -s)"
 chmod 600 $pk_filepath
-ssh-add $pk_filepath
 
 # create tar_path if it doesn't exist
 ssh -i $pk_filepath $DEPLOY_USER@$DEPLOY_HOST "mkdir -p $AWS_TAR_PATH"
