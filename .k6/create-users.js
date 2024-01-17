9d7c29d5be4f1d6b75938deb703983d9d337e039
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2024-01-17 11:54:04 -0300

    feat: implement create users k6

---
+++ .k6/create-users.js
@@ -0,0 +1,23 @@
+import { SharedArray } from "k6/data";
+import http from "k6/http";
+
+export const options = {
+  vus: 100,
+  duration: "10s",
+};
+
+const data = new SharedArray("users", function () {
+  const result = JSON.parse(open("./seed/new_users.json"));
+  return result;
+});
+
+export default function () {
+  const randomUser = data[Math.floor(Math.random() * data.length)];
+  const body = JSON.stringify({
+    username: randomUser.username,
+    avatar: randomUser.avatar_url,
+  });
+  const headers = { "Content-Type": "application/json" };
+
+  http.post("http://localhost:5144/Users", body, { headers });
+}
