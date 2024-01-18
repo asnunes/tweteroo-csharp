2cd240befd4d432e91d6c00dd4fb445df1459d53
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2024-01-18 19:08:25 -0300

    Update health.js thresholds

--- .k6/test/health.js
+++ .k6/test/health.js
@@ -4,8 +4,8 @@ export const options = {
   vus: 1000,
   duration: "10s",
   thresholds: {
-    http_req_duration: ["p(95)<200"],
-    http_req_failed: ["rate<0.01"],
+    http_req_duration: ["p(95)<2000"],
+    http_req_failed: ["rate<0.1"],
   },
 };
 
