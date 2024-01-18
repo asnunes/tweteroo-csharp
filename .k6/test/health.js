56a6333a13942d327df97bfe24227e901a1431aa
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2024-01-18 15:57:55 -0300

    feat: add pass/fail critearia to k6

--- .k6/test/health.js
+++ .k6/test/health.js
@@ -3,6 +3,10 @@ import http from "k6/http";
 export const options = {
   vus: 1000,
   duration: "10s",
+  thresholds: {
+    http_req_duration: ["p(95)<10"],
+    http_req_failed: ["rate<0.01"],
+  },
 };
 
 export default function () {
